7f0ef8d364ba37d7540cf923efceb8d1b9260469
createunique <142560870+createunique@users.noreply.github.com>
2024-04-24 14:06:48 +0530

    Update reset_password.php REMOVED GMAIL DETIALLS

--- password_reset/reset_password.php
+++ password_reset/reset_password.php
@@ -56,8 +56,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
             $mail->isSMTP();                                       // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                  // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                              // Enable SMTP authentication
-            $mail->Username   = 'tempkah01@gmail.com';             // SMTP username
-            $mail->Password   = 'jalskywnvpdwinnw';                // SMTP password
+            $mail->Username   = 'YOUR_GMAIL';             // SMTP username
+            $mail->Password   = 'GMAIL_PASSWORD';                // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;       // Enable implicit TLS encryption
             $mail->Port       = 465;                               // TCP port to connect to
 
